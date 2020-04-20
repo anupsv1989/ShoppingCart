6a747a9504230e2812188ed5f35c3eaf2c9bd3d7
anupsv1989 <anup.v@meratransport.com>
2020-04-20 22:13:01 +0530

    Updates

---
+++ _next/static/w-0AKccuzbiPkzdO0-x2G/pages/list.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{hKUc:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/list",function(){return o("oVyb")}])}},[["hKUc",0,1,2,4,6]]]);
